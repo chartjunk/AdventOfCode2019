27e21827a8d8cb71cd13272b5ea2791bfa85271b
Joona Luoma <joona.luoma@solteq.com>
2019-12-06 14:14:50 +0200

    Move P1 to library

--- Aoc2019.D3.P1/Library.fs
+++ Aoc2019.D3.P1/Library.fs
@@ -1,28 +1,22 @@
 ﻿namespace Aoc2019.D3.P1
 
-//open Aoc2019.Utils
-//open Aoc2019.Utils.Functional
-//open System
+open Aoc2019.Utils
+open Aoc2019.Utils.Functional
+open System
 
-//module Wires =
-//    type Dir = Hor=0 | Ver=1
-//    let absoluteMovements =
-//        Seq.fold (fun (x, y, ms) (next:string) -> let len = int next.[1..] in match next.[0] with
-//        | c&'U' | c&'D' -> let ny = (match c with | 'U' -> (+) | _ -> (-)) y len in x, ny, seq { yield! ms; yield (x, y, ny, Dir.Ver) }
-//        | c             -> let nx = (match c with | 'R' -> (+) | _ -> (-)) x len in nx, y, seq { yield! ms; yield (y, x, nx, Dir.Hor) }) (0, 0, Seq.empty)
-//        >> fun (_, _, ms) -> ms
-
-//    let absoluteMovementsByDir = absoluteMovements >> (fun ms -> ms |> Seq.filter(fth >> (=)Dir.Hor), ms |> Seq.filter(fth >> (=)Dir.Ver))
-
-//    let maybeIntersectionBy (hy:int, hx:int, hnx:int, _) (vx:int, vy:int, vny:int, _) =
-//        match Math.Min(hx, hnx) <= vx && Math.Max(hx, hnx) >= vx && Math.Min(vy, vny) <= hy && Math.Max(vy, vny) >= hy, (vx, hy) with
-//        | _, (0, 0) -> None
-//        | true, _   -> Some (vx, hy)
-//        | _         -> None
-
-//    let movementPairs ([w1; w2]:list<list<string>>) =    
-//        let (w1h, w1v) = absoluteMovementsByDir w1
-//        let (w2h, w2v) = absoluteMovementsByDir w2
-//        ((w1h, w2v), (w2h, w1v)) |> Pair.map ((<||)Seq.allPairs) ||> Seq.interleave
-
-//    let read = String.linesToStringList >> List.map String.csvToStringList
+module Wires =
+    let movements =
+        List.fold (fun (x, y, ms) (next:string) -> let len = int next.[1..] in match next.[0] with
+        | c&'U' | c&'D' -> let ny = (match c with | 'U' -> (+) | _ -> (-)) y len in x, ny, (x, y, ny, 0)::ms
+        | c             -> let nx = (match c with | 'R' -> (+) | _ -> (-)) x len in nx, y, (y, x, nx, 1)::ms) (0, 0, [])
+        >> thd >> List.rev
+    
+    let maybeIntersectionAt (hy:int, hx:int, hnx:int, _) (vx:int, vy:int, vny:int, _) =
+        match Math.Min(hx, hnx) <= vx && Math.Max(hx, hnx) >= vx && Math.Min(vy, vny) <= hy && Math.Max(vy, vny) >= hy, (vx, hy) with
+        | _, (0, 0) -> None
+        | true, p   -> Some p
+        | _         -> None
+    
+    let intersections [w1ms; w2ms] = w1ms |> List.collecti (fun w1ix w1m -> w2ms |> List.choosei (fun w2ix w2m -> match w1m, w2m with
+        | _ when w1ix < w2ix || fth w1m = fth w2m -> None
+        | w1i, w2j -> maybeIntersectionAt w1i w2j |> Option.map (fun p -> (p, (w1ix, w2ix)))))
--- Aoc2019.D3.P1/Program.fs
+++ Aoc2019.D3.P1/Program.fs
@@ -1,26 +1,8 @@
 ﻿open System 
 open Aoc2019.Utils
-open Aoc2019.Utils.Functional
-
-type Dir = Hor=0 | Ver=1
-let movements =
-    List.fold (fun (x, y, ms) (next:string) -> let len = int next.[1..] in match next.[0] with
-    | c&'U' | c&'D' -> let ny = (match c with | 'U' -> (+) | _ -> (-)) y len in x, ny, (x, y, ny, Dir.Ver)::ms
-    | c             -> let nx = (match c with | 'R' -> (+) | _ -> (-)) x len in nx, y, (y, x, nx, Dir.Hor)::ms) (0, 0, [])
-    >> thd >> List.rev
-
-let maybeIntersectionAt (hy:int, hx:int, hnx:int, _) (vx:int, vy:int, vny:int, _) =
-    match Math.Min(hx, hnx) <= vx && Math.Max(hx, hnx) >= vx && Math.Min(vy, vny) <= hy && Math.Max(vy, vny) >= hy, (vx, hy) with
-    | _, (0, 0) -> None
-    | true, p   -> Some p
-    | _         -> None
+open Aoc2019.D3.P1.Wires
 
 let manhattanDistance (x:int) (y:int) = Math.Abs(x) + Math.Abs(y)
-
-let intersections [w1ms; w2ms] = w1ms |> List.collecti (fun w1ix w1m -> w2ms |> List.choosei (fun w2ix w2m -> match w1m, w2m with
-    | _ when w1ix < w2ix || fth w1m = fth w2m -> None
-    | w1i, w2j -> maybeIntersectionAt w1i w2j |> Option.map (fun p -> (p, (w1ix, w2ix)))))
-
 let nearestNonzeroIntersectionDistance = intersections >> Seq.map(fst >> (<||)manhattanDistance) >> Seq.min
 
 [<EntryPoint; STAThread>]
