6fe775757750900fea827e77b98c23702417c511
Joona Luoma <joona.luoma@solteq.com>
2019-12-07 17:55:47 +0200

    Tune formatting

--- Aoc2019.D5.P1/Library.fs
+++ Aoc2019.D5.P1/Library.fs
@@ -11,7 +11,7 @@ module IntCode2 =
     let mode mps p = (float)mps % (10. ** ((float)p)) / (10. ** (float)(p - 1)) |> int
     let calcOp f mps p1 p2 p3 mem = let r p i = read p (mode mps i) mem in f (r p1 1) (r p2 2) |> int |> flip (write p3) mem
     let outputOp mps p1 = read p1 (mode mps 1)
-    let jumpIfOp f mps p1 p2 pos mem = let r p i = read p (mode mps i) mem in match r p1 1 |> f with | true -> r p2 2 | _ -> pos + 3
+    let jumpOp f mps p1 p2 pos mem = let r p i = read p (mode mps i) mem in match r p1 1 |> f with | true -> r p2 2 | _ -> pos + 3
     
     let execute input =
         let rec executeRec output pos mem =
@@ -29,6 +29,6 @@ module IntCode2 =
                     output, pos + 4, calcOp f mps p1 p2 p3 mem
                 | 3, p1::_                   -> output, pos + 2, write p1 input mem
                 | 4, p1::_                   -> (outputOp mps p1 mem)::output, pos + 2, mem
-                | o&(5|6), p1::p2::_         -> output, jumpIfOp (match o with | 5 -> (<>)0 | _ -> (=)0) mps p1 p2 pos mem, mem
+                | o&(5|6), p1::p2::_         -> output, jumpOp (match o with | 5 -> (<>)0 | _ -> (=)0) mps p1 p2 pos mem, mem
                 |||> executeRec
         executeRec [] 0 >> Seq.head
