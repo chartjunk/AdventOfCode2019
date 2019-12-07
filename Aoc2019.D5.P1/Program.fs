d9ff92da7c9eedb766487be4a698459b04265f01
Joona Luoma <joona.luoma@solteq.com>
2019-12-07 02:01:23 +0200

    Fix formatting

--- Aoc2019.D5.P1/Program.fs
+++ Aoc2019.D5.P1/Program.fs
@@ -6,8 +6,8 @@ let read source (memory:list<int>) = function | 0 -> memory.[source] | _ -> sour
 let write target = List.replace target
 
 let mode mps p = (float)mps % (10. ** ((float)p)) / (10. ** (float)(p - 1)) |> int
-let calcOp f mps p1 p2 p3 memory = let read p i = read p memory (mode mps i) in f (read p1 1) (read p2 2) |> flip (write p3) memory
-let outputOp modeps p1 memory = read p1 memory (mode modeps 1)
+let calcOp f mps p1 p2 p3 memory = let r p i = read p memory (mode mps i) in f (r p1 1) (r p2 2) |> flip (write p3) memory
+let outputOp mps p1 memory = read p1 memory (mode mps 1)
 
 let execute input =
     let rec executeRec output pos memory =
@@ -18,8 +18,8 @@ let execute input =
         | _  -> let instruction = (op % 10, tail) in match instruction with
             | 4, p1::_ -> (outputOp mps p1 memory)::output, pos + 2, memory
             | _        -> match instruction with
-                | o&1, ps | o&2, ps -> let p1::p2::p3::_ = ps in output, pos + 5, calcOp (match o with | 1 -> (+) | _ -> (*)) mps p1 p2 p3 memory
-                | 3, p1::_          -> output, pos + 2, write p1 input memory
+                | o&(1|2), p1::p2::p3::_ -> output, pos + 4, calcOp (match o with | 1 -> (+) | _ -> (*)) mps p1 p2 p3 memory
+                | 3, p1::_  -> output, pos + 2, write p1 input memory
             |||> executeRec
     executeRec [] 0 >> Seq.head
 
