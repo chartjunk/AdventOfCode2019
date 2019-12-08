7dd981ea27a248593456836ff9dbf13099853456
Joona Luoma <joona.luoma@solteq.com>
2019-12-08 23:36:15 +0200

    Fix formatting

--- Aoc2019.D4.P2/Program.fs
+++ Aoc2019.D4.P2/Program.fs
@@ -4,11 +4,15 @@ open Aoc2019.D4.P1.PasswordCracking
 
 let atLeastOnePairWithoutMoreInARow n = 
     let s = string n
-    let rec recurse = function
-    | r, _, ix when ix >= s.Length           -> match r with | 2 -> true | _ -> false
-    | r, c, ix when int s.[ix] <> c && r = 2 -> true    
-    | r, c, ix when int s.[ix] = c           -> recurse (r + 1, c, ix + 1)
-    | _, _, ix                               -> recurse (1, int s.[ix], ix + 1)
+    let rec recurse =
+        function
+        | r, _, ix when ix >= s.Length           -> match r with | 2 -> true | _ -> false
+        | r, c, ix when int s.[ix] <> c && r = 2 -> true
+        | x                                      ->
+            match x with
+            | r, c, ix when int s.[ix] = c -> r + 1, c, ix + 1
+            | _, _, ix                     -> 1, int s.[ix], ix + 1
+            |> recurse
     recurse (0, -1, 0)
 
 [<EntryPoint; STAThread>]
